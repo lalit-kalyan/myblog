0129a8d37b8fead228c55f5ac42e788888cb96ef
lalit <lalit.rishikalyan@gmail.com>
2022-04-01 23:22:31 +0530

    added

---
+++ src/projects.js
@@ -0,0 +1,101 @@
+export const featuredProjects = [
+  {
+    id: 1,
+    title: "Social Media ",
+    img: "/images/card01.jpg",
+  },
+  {
+    id: 2,
+    title: "Raj UI ",
+    img: "/images/card02.jpg",
+  },
+  {
+    id: 3,
+    title: "E-Commerce ",
+    img: "/images/card03.jpg",
+  },
+  {
+    id: 4,
+    title: "Mobile App ",
+    img: "/images/card04.jpg",
+  },
+  {
+    id: 5,
+    title: "Sweets E-com",
+    img: "/images/card05.jpg",
+  },
+  {
+    id: 6,
+    title: "Rahul Blogs",
+    img: "/images/card06.jpg",
+  },
+];
+
+export const webProjects = [
+  {
+    id: 1,
+    title: "Social Media ",
+    img: "/images/card01.jpg",
+  },
+  {
+    id: 2,
+    title: "Raj UI ",
+    img: "/images/card02.jpg",
+  },
+  {
+    id: 3,
+    title: "E-Commerce ",
+    img: "/images/card03.jpg",
+  },
+  {
+    id: 4,
+    title: "Mobile App ",
+    img: "/images/card04.jpg",
+  },
+];
+
+export const mobileProjects = [
+  {
+    id: 1,
+    title: "Social Media ",
+    img: "/images/card05.jpg",
+  },
+  {
+    id: 2,
+    title: "Raj UI ",
+    img: "/images/card04.jpg",
+  },
+  {
+    id: 3,
+    title: "E-Commerce ",
+    img: "/images/card03.jpg",
+  },
+];
+
+export const desingProjects = [
+  {
+    id: 1,
+    title: "Social Media ",
+    img: "/images/card02.jpg",
+  },
+  {
+    id: 2,
+    title: "Raj UI ",
+    img: "/images/card01.jpg",
+  },
+  {
+    id: 3,
+    title: "E-Commerce ",
+    img: "/images/card03.jpg",
+  },
+  {
+    id: 4,
+    title: "Mobile App ",
+    img: "/images/card04.jpg",
+  },
+  {
+    id: 5,
+    title: "Sweets E-com",
+    img: "/images/card05.jpg",
+  },
+];
